ae9d61701ac1f8811590e70aea12ca3b4bf67e46
SSubhimAA2004 <healerking@umbaravai.com>
2026-07-26 15:12:10 +0530

    Update api.js

--- js/api.js
+++ js/api.js
@@ -153,11 +153,9 @@ window.CTM.API = (function () {
 
                 method: "POST",
 
-                headers: {
-
-                    "Content-Type": "application/json"
-
-                },
+headers: {
+    "Content-Type": "text/plain;charset=utf-8"
+},
 
                 body: JSON.stringify(payload)
 
